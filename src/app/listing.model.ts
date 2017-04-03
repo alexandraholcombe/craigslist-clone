7165899daf53cc8cbeece5980053aa5439fc2620
Alexandra Holcombe and Derek Villars <alexandra+derek@>
2017-04-03 15:32:50 -0700

    Add product listings

---
+++ src/app/listing.model.ts
@@ -0,0 +1,3 @@
+export class Listing {
+  constructor(public title: string, public price: number, public location: string, public description: string, public categoryId: number);
+}
---
+++ src/app/listing.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Listing } from './listing.model';
+import { LISTINGS } from './mock-listings';
+
+@Injectable()
+export class ListingService {
+
+  constructor() { }
+
+  getListings() {
+    return LISTINGS;
+  }
+
+  getListingById(listingId){
+    for (var i = 0; i < LISTINGS.length; i++) {
+      if (LISTINGS[i].id === listingId) {
+        return LISTINGS[i];
+      }
+    }
+  }
+
+}
